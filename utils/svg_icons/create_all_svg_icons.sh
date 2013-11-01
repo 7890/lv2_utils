119be87bad300de7a03b78712c806ae8d49227e5
Thomas Brand <tom@trellis.ch>
2013-11-01 19:34:38 +0100

    add atom generic

--- utils/svg_icons/create_all_svg_icons.sh
+++ utils/svg_icons/create_all_svg_icons.sh
@@ -14,6 +14,10 @@
 ./create_svg_icon.sh float_in.svg control_input_float
 ./create_svg_icon.sh float_out.svg control_output_float
 
-./create_svg_icon.sh atom_in.svg atom_input_midi
-./create_svg_icon.sh atom_out.svg atom_output_midi
+./create_svg_icon.sh atom_in_midi.svg atom_input_midi
+./create_svg_icon.sh atom_out_midi.svg atom_output_midi
+
+./create_svg_icon.sh atom_in_generic.svg atom_input_generic
+./create_svg_icon.sh atom_out_generic.svg atom_output_generic
+
 
