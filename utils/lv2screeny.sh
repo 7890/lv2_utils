0bd04db17a7e35c1814807d08fee0ab440474205
Thomas Brand <tom@trellis.ch>
2013-11-04 19:26:06 +0100

    always use generic gui. annotate screenshot with plugin uri.

--- utils/lv2screeny.sh
+++ utils/lv2screeny.sh
@@ -3,6 +3,9 @@
 #lv2ls | while read line; do ./autoscreenshot.sh "$line"; sleep 1; done
 #//tb/131029
 
+#use always generic ui for now
+jalv_cmd="jalv.gtk --generic-ui"
+
 if [ $# -eq 0 ]
 then
         echo "need param: lv2 plugin uri"
@@ -32,13 +35,23 @@ filename="$stripped.png"
 
 scrot --delay 2 --focused "$outputdir"/"$filename"_.png &
 #jalv.gtk http://gareus.org/oss/lv2/meters#NORstereo_gtk &
-jalv.gtk $1 &
+$jalv_cmd $1 &
 pid=$!
 sleep 3
 kill -9 $pid
 
 #cut unreasonable menu
 convert "$outputdir"/"$filename"_.png -crop +2+29 +repage "$outputdir"/"$filename"
-rm -f "$outputdir"/"$filename"_.png
+
+#http://www.imagemagick.org/Usage/annotating/
+#write uri to image footer
+width=`identify -format %w "$outputdir"/"$filename"`; \
+convert -background '#0008' -fill white -gravity center -size ${width}x30 \
+caption:"$1" \
+"$outputdir"/"$filename" +swap -gravity south -composite \
+"$outputdir"/"$filename"_.png
+
+mv "$outputdir"/"$filename"_.png "$outputdir"/"$filename"
 
 ls -1 "$outputdir"/"$filename"
+
